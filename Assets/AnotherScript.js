f22f3de9c62788d18472cea78f511580b70156ea
Alan Wang <alanwang093@gmail.com>
2024-10-19 00:06:16 -0700

    changed script to change text dynamically with a button press

--- Assets/AnotherScript.js
+++ Assets/AnotherScript.js
@@ -1,11 +1,12 @@
 
 //@input Asset.RemoteServiceModule remoteServiceModule
+//@input Component.Text3D text3DComponent
 
-var SIK = require("SpectaclesInteractionKit/SIK").SIK;
-var handInputData = SIK.HandInputData;
+const SIK = require("SpectaclesInteractionKit/SIK").SIK;
+const handInputData = SIK.HandInputData;
 
-var rightHand = handInputData.getHand("right");
-var leftHand = handInputData.getHand("left");
+const rightHand = handInputData.getHand("right");
+const leftHand = handInputData.getHand("left");
 
 if (rightHand.isTracked() || leftHand.isTracked()) {
     print("yay hands!");
@@ -16,31 +17,66 @@ if (rightHand.isTracked() || leftHand.isTracked()) {
 //can also do something like rightHand.isPinching()
 
 //connecting a button
-var good = function () {
-    print("gooooood");
-    fetchCatFact();
-    adiAPICall();
+const good = function () {
+    catFactChangeText();
+    // print("gooooood");
+    // fetchCatFact();
+    // adiAPICall();
+    // adiDoctorPost();
 }
 
-function fetchCatFact() {
+function catFactChangeText() {
     if (!script.remoteServiceModule) {
         print("RemoteServiceModule is not assigned!");
         return;
     }
 
-    var httpRequest = RemoteServiceHttpRequest.create();
+    if (!script.text3DComponent) {
+        print("Text3D component is not assigned!");
+        return;
+    }
+
+    const httpRequest = RemoteServiceHttpRequest.create();
     httpRequest.url = 'https://catfact.ninja/fact';
     httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
 
     script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
         if (response.statusCode === 200) {
-            var data = JSON.parse(response.body);
+            const data = JSON.parse(response.body);
             print('Random Cat Fact: ' + data.fact);
+
+            if (data.fact) {
+                script.text3DComponent.text = data.fact;
+                print('Updated Text3D with: ' + data.fact);
+            } else {
+                print('No text field found in response.');
+            }
         } else {
             print('Failed to fetch cat fact. Status code: ' + response.statusCode);
         }
     });
 }
+
+//WORKS
+// function fetchCatFact() {
+//     if (!script.remoteServiceModule) {
+//         print("RemoteServiceModule is not assigned!");
+//         return;
+//     }
+
+//     const httpRequest = RemoteServiceHttpRequest.create();
+//     httpRequest.url = 'https://catfact.ninja/fact';
+//     httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
+
+//     script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
+//         if (response.statusCode === 200) {
+//             const data = JSON.parse(response.body);
+//             print('Random Cat Fact: ' + data.fact);
+//         } else {
+//             print('Failed to fetch cat fact. Status code: ' + response.statusCode);
+//         }
+//     });
+// }
 
 function adiAPICall() {
     if (!script.remoteServiceModule) {
@@ -48,18 +84,48 @@ function adiAPICall() {
         return;
     }
 
-    var httpRequest = RemoteServiceHttpRequest.create();
+    const httpRequest = RemoteServiceHttpRequest.create();
     httpRequest.url = 'https://racer-grateful-shepherd.ngrok-free.app/startAppointment';
     httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
 
     script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
         if (response.statusCode === 200) {
-            var data = JSON.parse(response.body);
+            const data = JSON.parse(response.body);
             print('Your appointment id: ' + data.appointment_id);
         } else {
             print('Failed to get appointment id: ' + response.statusCode);
         }
     });
 }
+
+function adiDoctorPost() {
+    if (!script.remoteServiceModule) {
+        print("RemoteServiceModule is not assigned!");
+        return;
+    }
+
+    const httpRequest = RemoteServiceHttpRequest.create();
+    httpRequest.url = 'https://racer-grateful-shepherd.ngrok-free.app/startAppointment';
+    httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Post;
+
+    const requestBody = JSON.stringify({
+        doctor_id: 101010999
+    });
+    httpRequest.body = requestBody;
+
+    httpRequest.setHeader('Content-Type', 'application/json');
+
+    script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
+        if (response.statusCode === 200) {
+            const data = JSON.parse(response.body);
+            print('Your appointment id: ' + data.appointment_id);
+        } else {
+            print('Failed to get appointment id: ' + response.statusCode);
+        }
+    });
+}
+
+
+
 
 script.good = good;
