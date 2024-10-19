1a60c1a94ee3873da07b65e4afd2daeb9bf5cd7a
Alan Wang <alanwang093@gmail.com>
2024-10-19 11:59:49 -0700

    cleanup

--- Assets/SpeechToTextTranscription.js
+++ Assets/SpeechToTextTranscription.js
@@ -1,8 +1,12 @@
 //@input Asset.VoiceMLModule vmlModule {"label": "Voice ML Module"}
+//@input Asset.RemoteServiceModule remoteServiceModule
+
+const SIK = require("SpectaclesInteractionKit/SIK").SIK;
 
 var options = VoiceML.ListeningOptions.create();
 
 var accumulatedTranscript = "";
+var count = 5;
 
 var onListeningEnabledHandler = function () {
     script.vmlModule.startListening(options);
@@ -33,6 +37,30 @@ var onUpdateListeningEventHandler = function (eventArgs) {
       return;
     }
     accumulatedTranscript += eventArgs.transcription + ' ';
+    count -= 1;
+    print(count + ' is the count')
+    if (count === 0) {
+      print("within...")
+      const httpRequest = RemoteServiceHttpRequest.create();
+      httpRequest.url = 'https://racer-grateful-shepherd.ngrok-free.app/sendTranscript';
+      httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Post;
+      const reqBody = JSON.stringify({
+        transcript: accumulatedTranscript,
+        appointment_id: '123456789',
+      });
+      httpRequest.body = reqBody;
+      httpRequest.setHeader('Content-Type', 'application/json');
+
+      script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
+        if (response.statusCode === 200) {
+          print("transcript successfully sent!")
+        } else {
+          print("failed to send transcript. status code: " + response.statusCode)
+        }
+      })
+      accumulatedTranscript = "";
+      count = 5;
+    }
     print('Final Transcription: ' + eventArgs.transcription);
     print('Accumulated Transcription: ' + accumulatedTranscript);
 };
