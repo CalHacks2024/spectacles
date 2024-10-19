40596295a294046fe46e61e2c6f087bd007e4f83
Alan Wang <alanwang093@gmail.com>
2024-10-18 22:06:51 -0700

    added script 'AnotherScript.js' to make an API call

---
+++ Assets/AnotherScript.js
@@ -0,0 +1,46 @@
+
+//@input Asset.RemoteServiceModule remoteServiceModule
+
+var SIK = require("SpectaclesInteractionKit/SIK").SIK;
+var handInputData = SIK.HandInputData;
+
+var rightHand = handInputData.getHand("right");
+var leftHand = handInputData.getHand("left");
+
+if (rightHand.isTracked() || leftHand.isTracked()) {
+    print("yay hands!");
+} else {
+    print("no hands");
+}
+
+//can also do something like rightHand.isPinching()
+
+//connecting a button
+var good = function () {
+    print("gooooood");
+    fetchCatFact();
+}
+
+function fetchCatFact() {
+    if (!script.remoteServiceModule) {
+        print("RemoteServiceModule is not assigned!");
+        return;
+    }
+
+    var httpRequest = RemoteServiceHttpRequest.create();
+    httpRequest.url = 'https://catfact.ninja/fact';
+    httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Get;
+
+    script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
+        print('Response received: ' + response.body);
+
+        if (response.statusCode === 200) {
+            var data = JSON.parse(response.body);
+            print('Random Cat Fact: ' + data.fact);
+        } else {
+            print('Failed to fetch cat fact. Status code: ' + response.statusCode);
+        }
+    });
+}
+
+script.good = good;
--- Cache/TypeScript/Src/SpectaclesInteractionKit/Components/UI/PinchButton/PinchButton.ts
+++ Cache/TypeScript/Src/SpectaclesInteractionKit/Components/UI/PinchButton/PinchButton.ts
@@ -59,3 +59,5 @@ export class PinchButton extends BaseScriptComponent {
       }
   }
 }
+
+
