8b4ca37e6f7d65b5a4ec1d99a775a677e0f8568e
Alan Wang <alanwang093@gmail.com>
2024-10-20 10:55:28 -0700

    final touches before demo

--- Assets/StartStopRecord.js
+++ Assets/StartStopRecord.js
@@ -34,6 +34,8 @@ options.languageCode = 'en_US';
 options.shouldReturnAsrTranscription = true;
 options.shouldReturnInterimAsrTranscription = true;
 
+let appointment_id = null;
+
 var accumulatedTranscript = "";
 var count = 5;
 var isRecording = false;
@@ -95,38 +97,88 @@ function sendTranscript(transcript) {
 
     script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
         if (response.statusCode === 200) {
-            print("Transcript successfully sent!");
-            print(response.body.data)
-            if(response.body.data) {
-                print("here")
-                for(let i = 0; i < response.body.data.length; i++ ) {
-                    let id = response.body[i].id
-                    if(id == 1) {
-                        script.Window1.enabled = true;
-                        script.Window1text = response.body.data[i].body
-                        script.Window1title = response.body.data[i].title
-                    } else if(id == 2) {
-                        script.Window2.enabled = true;
-                        script.Window2text = response.body.data[i].body
-                        script.Window2title = response.body.data[i].title
-                    } else if (id == 3) {
-                        script.Window3.enabled = true;
-                        script.Window3text = response.body.data[i].body
-                        script.Window3title = response.body.data[i].title
-                    } else if (id == 4) {
-                        script.Window4.enabled = true;
-                        script.Window4text = response.body.data[i].body
-                        script.Window4title = response.body.data[i].title
-                    } else {
-                        script.Window5.enabled = true;
-                        script.Window5text = response.body.data[i].body
-                        script.Window5title = response.body.data[i].title
+            try {
+                print("Transcript successfully sent!");
+                print("HELLO ADI SUCKS")
+                print(response)
+                print("adi adi adi")
+                const res = JSON.parse(response.body)
+                print(res)
+                print("PARSED")
+                if(res !== null) {
+                    print("here")
+                    print(JSON.stringify(res))
+                    print(' ')
+                    res.data.forEach(item => {
+                        print(item)
+                        print(JSON.stringify(item))
+                        print("is the item")
+                        // if (item.id == 1) {
+                        //     print("HERE WE GO")
+                        //     // Check if Window1 and its text components are assigned
+                        //     if (script.Window1 && script.Window1text && script.Window1title) {
+                        //         print("insideeeee")
+                        //         script.Window1.enabled = true;
+                        //         script.Window1text.text = item.body;
+                        //         script.Window1title.text = item.title;
+                        //         print("OUT WE GO");
+                        //     } else {
+                        //         print("One or more components for Window1 are not assigned properly.");
+                        //     }
+                        // }
+                        switch (item.id) {
+                            case 1:
+                                script.Window1.enabled = true;
+                                script.Window1text.text = item.body;
+                                script.Window1title.text = item.title;
+                                break;
+                            case 2:
+                                script.Window2.enabled = true;
+                                script.Window2text.text = item.body;
+                                script.Window2title.text = item.title;
+                            break;
+                            case 3:
+                                script.Window3.enabled = true;
+                                script.Window3text.text = item.body;
+                                script.Window3title.text = item.title;
+                                break;
+                            case 4:
+                                script.Window4.enabled = true;
+                                script.Window4text.text = item.body;
+                                script.Window4title.text = item.title;
+                                break;
+                            default:
+                                break;
                     }
-                }   
-            }
+                })
+                // for(let i = 0; i < response.body.data.length; i++ ) {
+                //     let id = response.body[i].id
+                //     if(id == 1) {
+                //         script.Window1.enabled = true;
+                //         script.Window1text = response.body.data[i].body
+                //         script.Window1title = response.body.data[i].title
+                //     } else if(id == 2) {
+                //         script.Window2.enabled = true;
+                //         script.Window2text = response.body.data[i].body
+                //         script.Window2title = response.body.data[i].title
+                //     } else if (id == 3) {
+                //         script.Window3.enabled = true;
+                //         script.Window3text = response.body.data[i].body
+                //         script.Window3title = response.body.data[i].title
+                //     } else if (id == 4) {
+                //         script.Window4.enabled = true;
+                //         script.Window4text = response.body.data[i].body
+                //         script.Window4title = response.body.data[i].title
+                //     }
+                // }   
+        
         } else {
             print("Failed to send transcript. Status code: " + response.statusCode);
         }
+            } catch (error) {
+                console.log(error + ' is the error LOL');
+            }
+        }   
     });
 }
 
@@ -135,9 +187,54 @@ function toggleRecording() {
     if (isRecording) {
         //onListeningDisabledHandler(); // Stop listening
         updateButtonText("Start Recording");
+
+        const httpRequest = RemoteServiceHttpRequest.create();
+        httpRequest.url = 'https://racer-grateful-shepherd.ngrok-free.app/endAppointment';
+        httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Post;
+
+        const requestBody = JSON.stringify({
+            doctor_id: 63901323,
+            appointment_id: appointment_id,
+        });
+        httpRequest.body = requestBody;
+
+        httpRequest.setHeader('Content-Type', 'application/json');
+
+        script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
+            if (response.statusCode === 200) {
+                const data = JSON.parse(response.body);
+                print('Your appointment id: ' + data.appointment_id);
+                appointment_id = null;
+            } else {
+                print('Failed to get appointment id: ' + response.statusCode);
+            }
+        });
+
     } else {
         //onListeningEnabledHandler(); // Start listening
         updateButtonText("Stop Recording");
+
+        const httpRequest = RemoteServiceHttpRequest.create();
+        httpRequest.url = 'https://racer-grateful-shepherd.ngrok-free.app/startAppointment';
+        httpRequest.method = RemoteServiceHttpRequest.HttpRequestMethod.Post;
+
+        const requestBody = JSON.stringify({
+            doctor_id: 63901323
+        });
+        httpRequest.body = requestBody;
+
+        httpRequest.setHeader('Content-Type', 'application/json');
+
+        script.remoteServiceModule.performHttpRequest(httpRequest, function (response) {
+            if (response.statusCode === 200) {
+                const data = JSON.parse(response.body);
+                print('Your appointment id: ' + data.appointment_id);
+                appointment_id = data.appointment_id;
+            } else {
+                print('Failed to get appointment id: ' + response.statusCode);
+            }
+        });
+
         accumulatedTranscript = ""
     }
     
